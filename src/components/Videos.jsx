28d444e1e863070f96469b3fcd26756d9691514d
Adityapatil1203 <adityapatil9226@gmail.com>
2023-12-06 12:27:58 +0530

    first commit to VidHub

---
+++ src/components/Videos.jsx
@@ -0,0 +1,49 @@
+// import React from "react";
+// import { Stack, Box } from "@mui/material";
+
+// import ChannelCard from "./ChannelCard";
+// import VideoCard from "./VideoCard";
+
+// const Videos = ({ videos, direction }) => {
+  
+//   return (
+//     <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+//       {videos.map((item, idx) => (
+//         <Box key={idx}>
+//           {item.id.videoId && <VideoCard video={item} /> }
+//           {item.id.channelId && <ChannelCard channelDetail={item} />}
+//         </Box>
+//       ))}
+//     </Stack>
+//   );
+// }
+
+// export default Videos;
+import React from 'react'
+import VideoCard from './VideoCard'
+import ChannelCard from './ChannelCard'
+import { Stack , Box,  } from '@mui/material'
+import Loader from './Loader'
+
+const Videos = ({videos,direction}) => {
+
+    console.log("vidoesp :: ",videos);
+    if(!videos)  return <Loader/>;
+   
+  return (
+    <Stack direction={direction ||"row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+        { console.log("videos :: ",videos)}
+    {videos.map((item, idx) => (
+      <Box key={idx}>
+        {item.id.videoId && <VideoCard video={item} /> }
+        {item.id.channelId && <ChannelCard channelDetail={item} />}
+      </Box>
+    ))}
+  </Stack>
+   
+  )
+}
+
+export default Videos
+
+
